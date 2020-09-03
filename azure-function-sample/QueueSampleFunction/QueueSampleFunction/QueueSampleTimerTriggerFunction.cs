e15a98cc55c87deead3993e9e0fca32f81cca230
r44v <rs.raav@gmail.com>
2020-09-03 17:50:20 +0200

    Added sample queue function

---
+++ azure-function-sample/QueueSampleFunction/QueueSampleFunction/QueueSampleTimerTriggerFunction.cs
@@ -0,0 +1,26 @@
+/*
+ * Queue in function documentation
+ * https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-storage-queue
+ */
+
+
+using System;
+using Microsoft.Azure.Storage.Queue;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace QueueSampleFunction
+{
+    public static class QueueSampleTimerTriggerFunction
+    {
+        [FunctionName("QueueSampleTimerTriggerFunction")]
+        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log, [Queue("sample-queue")] CloudQueue queue)
+        {
+            var queueMsg = new CloudQueueMessage($"MsgForQueue at {DateTime.Now}");
+
+            queue.AddMessage(queueMsg);
+
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}

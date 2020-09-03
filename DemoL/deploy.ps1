62490b98f2e09029b4b04fa0e092804afc58a64d
Raav Schravesande <r.schravesande@wearetriple.com>
2020-09-03 16:48:35 +0200

    Removed all files from previous years

--- DemoL/deploy.ps1
+++
@@ -1,5 +0,0 @@
-Add-AzureRmAccount
-
-Select-AzureRmSubscription -SubscriptionName "Azure for Students"
-
-New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName Triple -TemplateFile "DeploymentTemplate.json" -Mode Complete

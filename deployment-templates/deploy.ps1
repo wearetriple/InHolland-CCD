706946b09f33a083a61e1468af47d979e1a7abfa
Raav Schravesande <r.schravesande@wearetriple.com>
2022-09-07 02:23:13 +0200

    feat: replaced arm with bicep

--- deployment-templates/deploy.ps1
+++ deployment-templates/deploy.ps1
@@ -1,12 +1,15 @@
-$prefix = 'inholland-h-'
-$suffix = '-' + (Get-Random -Minimum 1000 -Maximum 9999)
+$prefix              = 'INH-'+ (Get-Random -Minimum 1000 -Maximum 9999)
+$resource_group_name = "$prefix-WRKSHP-D-AZWE-RG-1"
+$template            = "./function.bicep"
 
-$resource_group_name = $prefix + 'rsgp' + $suffix
-$appserviceplan_name = $prefix + 'fasp' + $suffix
-$azure_function_name = $prefix + 'func' + $suffix
-$storageaccount_name = $prefix + 'stor' + $suffix
-# Storage account names cannot contain a dash
-$storageaccount_name = $storageaccount_name -replace '-', ''
+$parameters = @{
+    prefix      = $prefix
+    serviceTag  = "WRKSHP"
+    environment = "D"
+    regionTag   = "AZWE"
+}
+
+$parameters = $parameters.Keys.ForEach({"$_=$($parameters[$_])"}) -join ' '
 
 Write-Host "Deploying resources in $resource_group_name"
 
@@ -14,7 +17,6 @@ Write-Host "Deploying resources in $resource_group_name"
 az group create -l westeurope -n $resource_group_name
 
 # Deploy resources inside resource-group
-az deployment group create --mode Incremental --resource-group $resource_group_name --template-file template-azure-function.json --parameters appService_name=$azure_function_name appServicePlan_name=$appserviceplan_name resourceGroup=$resource_group_name storageaccount_name=$storageaccount_name
-
-#az deployment group create --resource-group tri-inholland --template-file azure-function.json --parameters @azure-function.env.parameters.json
-
+$cmd = "az deployment group create --mode Incremental --resource-group $resource_group_name --template-file $template --parameters $parameters"
+Write-Host $cmd
+Invoke-Expression  $cmd
